1641fc66a3ec118f89d0db85efff4f7baed4aa75
VVelosoS31 <79374598+VVelosoS31@users.noreply.github.com>
2021-09-15 22:46:33 -0300

    Criando a classe Scores
    Criando a classe Scores.java dentro do package game_entry

---
+++ game_entry/src/game_entry/Scores.java
@@ -0,0 +1,25 @@
+package game_entry;
+
+public class Scores {
+	
+	public static final int maxEntries = 10;
+	protected int numEntries;
+	protected GameEntry[] entries;
+	
+	public Scores() {
+		entries = new GameEntry[maxEntries];
+		numEntries = 0;
+	}
+	
+	public String toString() {
+		String s = "[";
+		for(int i = 0; i < numEntries; i++) {
+			if(i > 0) {
+				s += ", ";
+				s += entries[i];
+			}
+		}
+		return s + "]";
+	}
+
+}
